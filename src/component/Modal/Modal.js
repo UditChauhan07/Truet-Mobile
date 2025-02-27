102805a59c4f4a7682ec024bd7d799db0acc4a42
NirajDX <niraj.t@designersx.com>
2025-02-27 16:04:24 +0530

    [AP] Modal and Ui of offcanvas create (AP v1.1.7)

--- src/component/Modal/Modal.js
+++ src/component/Modal/Modal.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import styles from '../Modal/Modal.module.css'
 
+
+
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
   return (
     <div className={styles.modalOverlay} >
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <button onClick={onClose} className={styles.closeButton}>
-        <img src="svg/back-arrow.svg"/>
+        <img src="svg/Cross-Icon.svg"/>
         </button>
+     
         {children}
       </div>
     </div>
