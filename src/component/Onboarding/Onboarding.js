614137b68d9812ccda528300222494ab0eb63015
NirajDX <niraj.t@designersx.com>
2025-03-04 10:15:23 +0530

    [AP] password change (AP v1.1.1)

--- src/component/Onboarding/Onboarding.js
+++ src/component/Onboarding/Onboarding.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import styles from '../Onboarding/Onboarding.module.css'
+import React from 'react';
+import styles from '../Onboarding/Onboarding.module.css';
 
-
+const onboardingItems = [
+    { img: 'svg/marketing.svg', title: 'Marketing SharePoint', desc: 'All the Polyglass Marketing Resources you will need to start' },
+    { img: 'svg/global.svg', title: 'Global Entry', desc: 'Official Trusted Traveler Program | Department of Homeland Security' },
+    { img: 'svg/Polyflex.svg', title: 'Polyflex Datasheet', desc: 'Check this to understand all about the product in one go' },
+    { img: 'svg/elearning.svg', title: 'eLearning Resources', desc: 'Complete these courses from' },
+    { img: 'svg/sarc.svg', title: 'SARC Brochure', desc: 'Go through this Brochure to complete' }
+];
 
 const Onboarding = () => {
     return (
@@ -18,56 +24,30 @@ const Onboarding = () => {
                     <img src='images/cloudPng.png' alt='' />
                 </div>
             </div>
+
             <div className={styles.bodyMain}>
-                <div className={styles.bodypart}>
-                    <div> <img src='svg/marketing.svg' alt='' /></div>
-                    <div><h2>Marketing SharePoint</h2>
-                        <p>All the Polyglass Marketing Resources you will need to start</p>
+                {onboardingItems.map((item, index) => (
+                    <div key={index} className={styles.bodypart}>
+                        <div className={styles.iconLogo}><img src={item.img} alt='' /></div>
+                        <div>
+                            <h2>{item.title}</h2>
+                            <p>{item.desc}</p>
+                        </div>
+                        <div><img src='svg/start.svg' alt='' /></div>
                     </div>
-
-                    <div> <img src='svg/done.svg' alt='' /></div>
-
-                </div>
-                <div className={styles.bodypart}>
-                    <div> <img src='svg/global.svg' alt='' /></div>
-                    <div><h2>Global Entry</h2>
-                        <p>Official Trusted Traveler Program | Department of Homeland Security</p>
-                    </div>
-
-                    <div> <img src='svg/done.svg' alt='' /></div>
-
-                </div>
-                <div className={styles.bodypart}>
-                    <div> <img src='svg/Polyflex.svg' alt='' /></div>
-                    <div><h2>Polyflex Datasheet</h2>
-                        <p >Check this to understand all about the product in one go</p>
-                    </div>
-                    <div> <img src='svg/start.svg' alt='' /></div>
-                </div>
-                <div className={styles.bodypart}>
-                    <div> <img src='svg/elearning.svg' alt='' /></div>
-                    <div><h2>eLearning Resources</h2>
-                        <p>Complete these courses from</p>
-                    </div>
-                    <div> <img src='svg/start.svg' alt='' /></div>
-                </div>
-                <div className={styles.bodypart}>
-                    <div> <img src='svg/sarc.svg' alt='' /></div>
-                    <div><h2>SARC Brochure</h2>
-                        <p>Go through this Brochure to complete</p>
-                    </div>
-                    <div> <img src='svg/start.svg' alt='' /></div>
-
-                </div>
-                <p className={styles.signP}>These are the course onboarding screens. Explore courses or <b>skip</b>skip to sign up.</p>
+                ))}
+                <p className={styles.signP}>
+                    These are the course onboarding screens. Explore courses or <b>skip</b> to sign up.
+                </p>
             </div>
+
             <div className={styles.skipMain}>
-            <div className={styles.skipDiv}>
-                <div><p>Skip Now</p></div>
-            </div>
+                <div className={styles.skipDiv}>
+                    <div><p>Skip Now</p></div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Onboarding
+export default Onboarding;
